fad8fab3acba1a619e075eb7e0394e8c74a0311c
framedstone <leeweixuan39@gmail.com>
2026-04-15 15:21:32 +0800

    chore(keymap): removed binding leader to ;

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,7 +14,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	end
 end
 vim.opt.rtp:prepend(lazypath)
-vim.g.mapleader = ';'
+-- vim.g.mapleader = ';'
 
 -- Setup lazy.nvim
 require('lazy').setup({
